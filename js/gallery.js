cc275ba401db881613908f4fb06b16c4db429dce
Maria Shvyrkova <shvyrkovamaria@gmail.com>
2021-04-04 20:30:27 +0300

    Update  script

--- js/gallery.js
+++ js/gallery.js
@@ -3,13 +3,21 @@ import itemsArr from "/data/gallery-items.js";
 const refs = {
   gallery: document.querySelector(".js-gallery"),
   lightbox: document.querySelector(".js-lightbox"),
-  content: document.querySelector(".lightbox__content"),
+  overlay: document.querySelector(".lightbox__overlay"),
   image: document.querySelector(".lightbox__image"),
-  btnClose: document.querySelector(".lightbox__button"),
+  btnClose: document.querySelector('button[data-action="close-lightbox"]'),
+  btnPrev: document.querySelector('button[data-action="previous"]'),
+  btnNext: document.querySelector('button[data-action="next"]'),
 };
 
 const galleryItems = createGalleryMarkup(itemsArr);
 refs.gallery.insertAdjacentHTML("beforeend", galleryItems);
+
+refs.gallery.addEventListener("click", onGalleryImageClick);
+refs.btnClose.addEventListener("click", onBtnCloseClick);
+refs.overlay.addEventListener("click", onLightboxOverlayClick);
+refs.btnPrev.addEventListener("click", onBtnPrevClick);
+refs.btnNext.addEventListener("click", onBtnNextClick);
 
 function createGalleryMarkup(items) {
   return items
@@ -34,7 +42,6 @@ function createGalleryMarkup(items) {
     .join("");
 }
 
-refs.gallery.addEventListener("click", onGalleryImageClick);
 function onGalleryImageClick(event) {
   event.preventDefault();
   if (!event.target.classList.contains("gallery__image")) {
@@ -42,10 +49,33 @@ function onGalleryImageClick(event) {
   }
   refs.lightbox.classList.add("is-open");
   refs.image.src = event.target.dataset.source;
+  window.addEventListener("keydown", onEscapePress);
 }
 
-refs.btnClose.addEventListener("click", onBtnCloseClick);
 function onBtnCloseClick() {
   refs.lightbox.classList.remove("is-open");
   refs.image.src = "";
+  window.removeEventListener("keydown", onEscapePress);
 }
+
+function onLightboxOverlayClick(event) {
+  if (event.currentTarget === event.target) {
+    onBtnCloseClick();
+  }
+}
+
+function onEscapePress(event) {
+  if (event.code === "Escape") {
+    onBtnCloseClick();
+  }
+}
+
+// function onBtnPrevClick() {
+//   const test = document.querySelector(".gallery__image");
+//   if ((refs.image.src = test.dataset.source)) {
+//     return (refs.image.src = test.dataset.source);
+//   }
+// }
+// function onBtnNextClick() {}
+//elem.previousSibling
+//elem.nextSibling
